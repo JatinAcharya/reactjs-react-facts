74a0cf88f52e5a04a0a905211cee74e4f7c41bfd
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-14 01:33:44 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <>
+      {/* <MainPage /> */}
+      <Navbar />
+      <Main />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navigation-bar">
+        <img src="/src/assets/react.svg" alt="React Logo" />
+        <h2>ReactFacts</h2>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;

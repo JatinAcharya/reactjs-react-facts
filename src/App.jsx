2f898786522c04483abba3e70cdef00a39eee25a
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-13 22:24:26 +0530

    native way of writing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>This is from App.jsx</div>;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-const MainContent = () => {
-  return <h1>React is Great!</h1>;
-};
+// const MainContent = () => {
+//   return <h1>React is Great!</h1>;
+// };
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <App /> */}
-    <MainContent />
-  </StrictMode>
-);
+let h1Element = document.createElement("h1");
+h1Element.textContent = "Hello React!";
+h1Element.className = "header";
+const rootElement = document.getElementById("root");
+rootElement.appendChild(h1Element);
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//     {/* <MainContent /> */}
+//   </StrictMode>
+// );

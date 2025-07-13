9e4d43a08404d95891a12ef74dfef38827b3c10e
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-14 00:21:59 +0530

    custom page component challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>This is from App.jsx</div>;
+  console.log(<h1>Hello world</h1>);
+  return (
+    <main>
+      <div className="heading">
+        <img src="/src/assets/react.svg" alt="React-Logo" />
+        <h2>React Facts</h2>
+      </div>
+      <div className="main-content">
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created bt Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is mainted by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </main>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
-// const MainContent = () => {
-//   return <h1>React is Great!</h1>;
-// };
+// // const MainContent = () => {
+// //   return <h1>React is Great!</h1>;
+// // };
 
-let h1Element = document.createElement("h1");
-h1Element.textContent = "Hello React!";
-h1Element.className = "header";
-const rootElement = document.getElementById("root");
-rootElement.appendChild(h1Element);
+// // const h1Element = document.createElement("h1");
+// // h1Element.textContent = "Hello React!";
+// // h1Element.className = "header";
+// // const rootElement = document.getElementById("root");
+// // rootElement.appendChild(h1Element);
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//     {/* <MainContent /> */}
-//   </StrictMode>
-// );
+const PageComponent = () => {
+  return (
+    <ol>
+      <li>Reason 1</li>
+      <li>Reason 2</li>
+      <li>Reason 3</li>
+      <li>Reason 4</li>
+    </ol>
+  );
+};
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    {/* <MainContent /> */}
+    <PageComponent />
+  </StrictMode>
+);
